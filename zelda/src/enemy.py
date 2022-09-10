585496b647b9674c526b9f756226922326c4f0e9
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2022-09-10 18:20:16 +0100

    upgrade init

--- zelda/src/enemy.py
+++ zelda/src/enemy.py
@@ -5,7 +5,7 @@ from support import *
 from player import Player
 
 class Enemy(Entity):
-    def __init__(self, monster_name, pos, groups, obstacle_sprites, damage_player, trigger_death_particles):
+    def __init__(self, monster_name, pos, groups, obstacle_sprites, damage_player, trigger_death_particles, add_exp):
         super().__init__(groups)
         self.sprite_type = 'enemy'
 
@@ -38,6 +38,7 @@ class Enemy(Entity):
         self.attack_cooldown = 400
         self.damage_player = damage_player
         self.trigger_death_particles = trigger_death_particles
+        self.add_exp = add_exp
 
         # invincility timer
         self.vulnerable = True
@@ -124,9 +125,10 @@ class Enemy(Entity):
             self.vulnerable = False
         
     def check_death(self):
-        if self.health <= 0:            
+        if self.health <= 0:
             self.kill()
             self.trigger_death_particles(self.rect.center,self.monster_name)
+            self.add_exp(self.exp)
 
     def hit_reaction(self):
         if not self.vulnerable:
---
+++ zelda/src/upgrade.py
@@ -0,0 +1,43 @@
+import pygame
+from settings import *
+from player import Player
+
+class Upgrade:
+    def __init__(self, player: Player):
+        self.display_surface = pygame.display.get_surface()
+        self.player = player
+        self.attribute_nr = len(player.stats)
+        self.attribute_names = list(player.stats.keys())
+        self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
+
+        # selection system
+        self.selection_index = 0
+        self.selection_time = None
+        self.can_move = True
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if self.can_move:
+            if keys[pygame.K_RIGHT] and self.selection_index < self.attribute_nr - 1:
+                self.selection_index += 1
+                self.can_move = False
+                self.selection_time = pygame.time.get_ticks()
+            elif keys[pygame.K_LEFT]  and self.selection_index > 0:
+                self.selection_index -= 1
+                self.can_move = False
+                self.selection_time = pygame.time.get_ticks()
+
+            if keys[pygame.K_SPACE]:                
+                self.can_move = False
+                self.selection_time = pygame.time.get_ticks()
+                print(self.selection_index)
+
+    def selection_cooldown(self):
+        if not self.can_move:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.selection_time >= 300:
+                self.can_move = True
+
+    def display(self):        
+        self.input()
+        self.selection_cooldown()
