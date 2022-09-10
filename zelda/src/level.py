1622da86ecff2da4610187ffbcfc738e6fcfde70
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2022-09-10 17:52:09 +0100

    magic

--- zelda/src/level.py
+++ zelda/src/level.py
@@ -9,6 +9,7 @@ from debug import debug
 from ui import UI
 from enemy import Enemy
 from particles import AnimationPlayer
+from magic import MagicPlayer
 
 class Level:
     def __init__(self):
@@ -33,6 +34,7 @@ class Level:
 
         # particles
         self.animation_player = AnimationPlayer()
+        self.magic_player = MagicPlayer(self.animation_player)
 
     def create_map(self):
         layout = {
@@ -93,9 +95,11 @@ class Level:
         self.current_attack = Weapon(self.player,[self.visible_sprites, self.attack_sprites])
 
     def create_magic(self, style, strength, cost):
-        print(f'style: {style}')
-        print(f'strength: {strength}')
-        print(f'cost: {cost}')
+        if style == 'heal':
+            self.magic_player.heal(self.player, strength, cost, [self.visible_sprites])
+        
+        if style == 'flame':
+            self.magic_player.flame(self.player, cost, [self.visible_sprites, self.attack_sprites])
 
     def destroy_attack(self):
         if self.current_attack:
