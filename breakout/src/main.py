0d8a22b9d0875ba113f5857d2a5691c742fbc5f8
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2022-09-13 21:45:12 +0100

    bg

--- breakout/src/main.py
+++ breakout/src/main.py
@@ -1,3 +1,4 @@
+from turtle import width
 import pygame,sys,time
 from settings import *
 
@@ -8,6 +9,17 @@ class Game:
 		pygame.init()
 		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 		pygame.display.set_caption(GAME_NAME)
+
+		# bg
+		self.bg = self.create_bg()
+
+	def create_bg(self):
+		bg_original = pygame.image.load('../graphics/other/bg.png').convert()		
+		scale_factor = WINDOW_HEIGHT / bg_original.get_height()
+		scaled_w = bg_original.get_width() * scale_factor
+		scaled_h = bg_original.get_height() * scale_factor
+		scaled_bg = pygame.transform.scale(bg_original,(scaled_w,scaled_h))		
+		return scaled_bg
 
 	def run(self):
 		last_time = time.time()
@@ -23,6 +35,9 @@ class Game:
 					pygame.quit()
 					sys.exit()				
 
+			# draw the frame
+			self.display_surface.blit(self.bg,(0,0))
+			
 			# update window
 			pygame.display.update()
 
