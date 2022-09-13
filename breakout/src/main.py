083a520f2418bd6c9b18a1c258ddd03016c1fd2a
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2022-09-13 21:34:50 +0100

    naming

--- breakout/src/main.py
+++ breakout/src/main.py
@@ -7,7 +7,7 @@ class Game:
 		# general setup
 		pygame.init()
 		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-		pygame.display.set_caption('Breakout')
+		pygame.display.set_caption(GAME_NAME)
 
 	def run(self):
 		last_time = time.time()
