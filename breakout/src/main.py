35e72d0c476261f4d8d53f44c32900ed5680e14f
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2022-09-17 00:44:49 +0100

    blocks

--- breakout/src/main.py
+++ breakout/src/main.py
@@ -1,7 +1,7 @@
 from turtle import width
 import pygame,sys,time
 from settings import *
-from sprites import Player, Ball
+from sprites import Player, Ball, Block
 
 class Game:
 	def __init__(self):
@@ -16,10 +16,12 @@ class Game:
 
 		# sprite group setup
 		self.all_sprites = pygame.sprite.Group()
+		self.block_sprites = pygame.sprite.Group()
 		
 		# setup
 		self.player = Player(self.all_sprites)
-		self.ball = Ball(self.all_sprites, self.player)
+		self.stage_setup()
+		self.ball = Ball(self.all_sprites, self.player, self.block_sprites)
 
 	def create_bg(self):
 		bg_original = pygame.image.load('../graphics/other/bg.png').convert()		
@@ -28,6 +30,15 @@ class Game:
 		scaled_h = bg_original.get_height() * scale_factor
 		scaled_bg = pygame.transform.scale(bg_original,(scaled_w,scaled_h))		
 		return scaled_bg
+
+	def stage_setup(self):
+		# cycle block map
+		for row_index, row in enumerate(BLOCK_MAP):
+			for col_index, col in enumerate(row):
+				if col != ' ':
+					y = row_index * (BLOCK_HEIGHT + GAP_SIZE) + GAP_SIZE // 2
+					x = col_index * (BLOCK_WIDTH + GAP_SIZE) + GAP_SIZE // 2						
+					Block(col,(x,y),[self.all_sprites, self.block_sprites])
 
 	def run(self):
 		last_time = time.time()
