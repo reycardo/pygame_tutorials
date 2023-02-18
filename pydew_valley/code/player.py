ce7158ef5dd3c6ee1abed49268a1ca12a145021e
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2023-02-18 21:29:06 +0000

    collision and trees

--- pydew_valley/code/player.py
+++ pydew_valley/code/player.py
@@ -4,7 +4,7 @@ from support import *
 from timer_support import Timer
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self, pos, group):
+	def __init__(self, pos, group, collision_sprites, tree_sprites):
 		super().__init__(group)
 
 		self.import_assets()
@@ -20,6 +20,10 @@ class Player(pygame.sprite.Sprite):
 		self.direction = pygame.math.Vector2()
 		self.pos = pygame.math.Vector2(self.rect.center)
 		self.speed = 400
+		
+		# collision
+		self.hitbox = self.rect.copy().inflate((-126,-70))
+		self.collision_sprites = collision_sprites
 
 		# timers
 		self.timers = {
@@ -39,8 +43,33 @@ class Player(pygame.sprite.Sprite):
 		self.seed_index = 0
 		self.selected_seed = self.seeds[self.seed_index]
 
+		# inventory
+		self.item_invetory = {
+			'wood': 0,
+			'apple': 0,
+			'corn': 0,
+			'tomato': 0,
+		}
+
+		# interaction
+		self.tree_sprites = tree_sprites
+
 	def use_tool(self):
-		pass
+		if self.selected_tool == 'hoe':
+			pass
+
+		if self.selected_tool == 'axe':			
+			for tree in self.tree_sprites.sprites():
+				if tree.rect.collidepoint(self.target_pos):
+					tree.damage()
+
+		if self.selected_tool == 'water':
+			pass
+
+	
+	def get_target_pos(self):
+		
+		self.target_pos = self.rect.center + PLAYER_TOOL_OFFSET[self.status.split('_')[0]]
 
 	def use_seed(self):
 		pass
@@ -127,6 +156,27 @@ class Player(pygame.sprite.Sprite):
 		for timer in self.timers.values():
 			timer.update()
 
+	def collision(self, direction):
+		for sprite in self.collision_sprites.sprites():
+			if hasattr(sprite, 'hitbox'):
+				if sprite.hitbox.colliderect(self.hitbox):
+					if direction == 'horizontal':
+						if self.direction.x > 0: # moving to the right
+							self.hitbox.right = sprite.hitbox.left
+						if self.direction.x < 0: # moving to the left
+							self.hitbox.left = sprite.hitbox.right
+						self.rect.centerx = self.hitbox.centerx
+						self.pos.x = self.hitbox.centerx				
+					
+					if direction == 'vertical':
+						if self.direction.y > 0: # moving down
+							self.hitbox.bottom = sprite.hitbox.top
+						if self.direction.y < 0: # moving up
+							self.hitbox.top = sprite.hitbox.bottom
+						self.rect.centery = self.hitbox.centery
+						self.pos.y = self.hitbox.centery
+				
+
 	def move(self, dt):
 
 		# normalizing a vector
@@ -135,15 +185,20 @@ class Player(pygame.sprite.Sprite):
 
 		# horizontal movement
 		self.pos.x += self.direction.x * self.speed * dt
-		self.rect.centerx = self.pos.x
+		self.hitbox.centerx = round(self.pos.x)
+		self.rect.centerx = self.hitbox.centerx
+		self.collision('horizontal')
 		
 		# vertical movement        
 		self.pos.y += self.direction.y * self.speed * dt
-		self.rect.centery = self.pos.y
+		self.hitbox.centery = round(self.pos.y)
+		self.rect.centery = self.hitbox.centery
+		self.collision('vertical')
 
 	def update(self, dt):
 		self.input()
 		self.get_status()
 		self.update_timers()
+		self.get_target_pos()
 		self.move(dt)
 		self.animate(dt)
--- pydew_valley/code/sprites.py
+++ pydew_valley/code/sprites.py
@@ -1,6 +1,7 @@
 import pygame
-from support import import_folder
 from settings import *
+from random import randint, choice
+from timer_support import Timer
 
 class Generic(pygame.sprite.Sprite):
 	def __init__(self, pos, surf: pygame.surface.Surface, groups, z = LAYERS['main']):
@@ -8,6 +9,7 @@ class Generic(pygame.sprite.Sprite):
 		self.image = surf
 		self.rect = self.image.get_rect(topleft = pos)
 		self.z = z
+		self.hitbox = self.rect.copy().inflate(self.rect.width * 0.2, -self.rect.height * 0.75)
 
 class Water(Generic):
 	def __init__(self, pos, frames, groups):
@@ -24,18 +26,6 @@ class Water(Generic):
 			z = LAYERS['water']
 		)
 
-	def import_assets(self):		
-		self.animations = {
-			 'up': [], 'down': [], 'left': [], 'right': [],
-			 'up_idle': [], 'down_idle': [], 'left_idle': [], 'right_idle': [],
-			 'up_hoe': [], 'down_hoe': [], 'left_hoe': [], 'right_hoe': [],
-			 'up_axe': [], 'down_axe': [], 'left_axe': [], 'right_axe': [],
-			 'up_water': [], 'down_water': [], 'left_water': [], 'right_water': []
-		}
-
-		for animation in self.animations:
-			full_path = '../graphics/character/' + animation
-			self.animations[animation] = import_folder(full_path)
 
 	def animate(self, dt):
 		self.frame_index += 5 *dt
@@ -50,11 +40,94 @@ class Water(Generic):
 class WildFlower(Generic):
 	def __init__(self, pos, surf, groups):
 		super().__init__(pos = pos, surf = surf, groups = groups)	
+		self.hitbox = self.rect.copy().inflate(-20, -self.rect.height * 0.9)
+
+class Particle(Generic):
+	def __init__(self, pos, surf: pygame.surface.Surface, groups, z, duration = 200):
+		super().__init__(pos, surf, groups, z)
+		self.start_time = pygame.time.get_ticks()
+		self.duration = duration
+
+		# white surface
+		mask_surface = pygame.mask.from_surface(self.image)
+		new_surf = mask_surface.to_surface()
+		new_surf.set_colorkey((0,0,0))
+		self.image = new_surf
+
+	
+	def update(self, dt):
+		current_time = pygame.time.get_ticks()
+		if current_time - self.start_time > self.duration:
+			self.kill()
 
 class Tree(Generic):
-	def __init__(self, pos, surf, groups, name):
+	def __init__(self, pos, surf, groups, name, player_add):
 		super().__init__(
 			pos = pos, 
 			surf = surf, 
 			groups = groups			
-		)	
+		)
+		# tree attributes
+		self.name = name
+		self.health = 5
+		self.alive = True
+		stump_path = f'../graphics/stumps/{"small" if self.name == "Small" else "large"}.png'
+		self.stump_surf = pygame.image.load(stump_path).convert_alpha()
+		self.invul_timer = Timer(200)
+
+		# apples
+		self.apple_surf = pygame.image.load('../graphics/fruit/apple.png').convert_alpha()
+		self.apple_pos = APPLE_POS[self.name]
+		self.apple_sprites = pygame.sprite.Group()
+		self.create_fruit()
+
+		self.player_add = player_add
+
+	def damage(self):
+
+		# damaging the tree
+		self.health -= 1
+
+		# remove and apple
+		if len(self.apple_sprites.sprites()) > 0:
+			random_apple = choice(self.apple_sprites.sprites())
+			Particle(
+				pos = random_apple.rect.topleft, 
+				surf = random_apple.image, 
+				groups = self.groups()[0], 
+				z = LAYERS['fruit']
+			)
+			self.player_add('apple', 1)
+			random_apple.kill()
+
+	def check_death(self):
+		if self.health <= 0:
+			Particle(
+				pos = self.rect.topleft, 
+				surf = self.image, 
+				groups = self.groups()[0], 
+				z = LAYERS['fruit'],
+				duration = 300
+			)			
+			self.image = self.stump_surf
+			self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+			self.hitbox = self.rect.copy().inflate(-10,-self.rect.height * 0.6)
+			self.alive = False
+			amount = 1 if self.name == "Small" else 2
+			self.player_add('wood', amount)
+
+	def update(self, dt):
+		if self.alive:
+			self.check_death()
+
+	def create_fruit(self):
+		for pos in self.apple_pos:
+			if randint(0,10) < 2:
+				x = pos[0] + self.rect.left
+				y = pos[1] + self.rect.top
+				Generic(
+					pos = (x,y), 
+					surf = self.apple_surf, 
+					groups = [self.apple_sprites, self.groups()[0]],
+					z = LAYERS['fruit']
+				)
