312deafafdc0aa25423f7bb1e550a9768536ed5f
ricardo-lopes3 <ricardo.lopes3@vodafone.com>
2023-02-19 03:51:57 +0000

    sky, soil, transition

--- pydew_valley/code/level.py
+++ pydew_valley/code/level.py
@@ -2,9 +2,13 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic, Water, WildFlower, Tree
+from sprites import Generic, Water, WildFlower, Tree, Interaction
 from pytmx.util_pygame import load_pygame
 from support import *
+from transition import Transition
+from soil import SoilLayer
+from sky import Rain
+from random import randint
 
 class Level:
 	def __init__(self):
@@ -16,9 +20,17 @@ class Level:
 		self.all_sprites = CameraGroup()
 		self.collision_sprites = pygame.sprite.Group()
 		self.tree_sprites = pygame.sprite.Group()
+		self.interaction_sprites = pygame.sprite.Group()
 
+		self.soil_layer = SoilLayer(self.all_sprites)
 		self.setup()
 		self.overlay = Overlay(self.player)
+		self.transition = Transition(self.reset, self.player)
+
+		# sky
+		self.rain = Rain(self.all_sprites)
+		self.raining = randint(0,10) > 3
+		self.soil_layer.raining = self.raining
 
 	def setup(self):
 		tmx_data = load_pygame('../data/map.tmx')
@@ -59,15 +71,24 @@ class Level:
 		for x,y,surf in tmx_data.get_layer_by_name('Collision').tiles():
 			Generic((x*TILE_SIZE, y*TILE_SIZE), pygame.Surface((TILE_SIZE,TILE_SIZE)), self.collision_sprites)
 
-		# PLayer
+		# Player
 		for obj in tmx_data.get_layer_by_name('Player'):
 			if obj.name == 'Start':
 				self.player = Player(
 					pos = (obj.x,obj.y),
 					group = self.all_sprites, 
 					collision_sprites = self.collision_sprites,
-					tree_sprites = self.tree_sprites)
-		
+					tree_sprites = self.tree_sprites,
+					interaction = self.interaction_sprites,
+					soil_layer = self.soil_layer)
+
+			if obj.name == 'Bed':
+				Interaction(
+					pos = (obj.x,obj.y),
+					size = (obj.width,obj.height),
+					groups = self.interaction_sprites, 					
+					name = obj.name)
+
 		Generic(
 			pos = (0,0), 
 			surf = pygame.image.load('../graphics/world/ground.png').convert_alpha(), 
@@ -77,11 +98,38 @@ class Level:
 	def player_add(self, item, amount):
 		self.player.item_invetory[item] += amount
 
+	def reset(self):
+
+		# soil
+		self.soil_layer.remove_water()
+		
+		# randomize the rain
+		self.raining = randint(0,10) > 3
+		self.soil_layer.raining = self.raining
+		if self.raining:
+			self.soil_layer.water_all()
+
+		# apples on the trees
+		for tree in self.tree_sprites.sprites():
+			for apple in tree.apple_sprites.sprites():
+				apple.kill()
+			tree.create_fruit()
+
 	def run(self,dt):
 		self.display_surface.fill('black')
 		self.all_sprites.custom_draw(self.player)
 		self.all_sprites.update(dt)
+		
 		self.overlay.display()
+
+		# rain
+		if self.raining:
+			self.rain.update()
+
+		# transition overlay
+		if self.player.sleep:
+			self.transition.play()
+
 
 class CameraGroup(pygame.sprite.Group):
 	def __init__(self):
--- pydew_valley/code/player.py
+++ pydew_valley/code/player.py
@@ -4,7 +4,7 @@ from support import *
 from timer_support import Timer
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self, pos, group, collision_sprites, tree_sprites):
+	def __init__(self, pos, group, collision_sprites, tree_sprites, interaction, soil_layer):
 		super().__init__(group)
 
 		self.import_assets()
@@ -53,10 +53,13 @@ class Player(pygame.sprite.Sprite):
 
 		# interaction
 		self.tree_sprites = tree_sprites
+		self.interaction = interaction
+		self.sleep = False
+		self.soil_layer = soil_layer
 
 	def use_tool(self):
 		if self.selected_tool == 'hoe':
-			pass
+			self.soil_layer.get_hit(self.target_pos)
 
 		if self.selected_tool == 'axe':			
 			for tree in self.tree_sprites.sprites():
@@ -64,7 +67,7 @@ class Player(pygame.sprite.Sprite):
 					tree.damage()
 
 		if self.selected_tool == 'water':
-			pass
+			self.soil_layer.water(self.target_pos)
 
 	
 	def get_target_pos(self):
@@ -97,7 +100,7 @@ class Player(pygame.sprite.Sprite):
 	def input(self):
 		keys = pygame.key.get_pressed()
 
-		if not self.timers['tool use'].active:
+		if not self.timers['tool use'].active and not self.sleep:
 			# directions
 			if keys[pygame.K_UP]:
 				self.direction.y = -1
@@ -143,6 +146,16 @@ class Player(pygame.sprite.Sprite):
 				self.seed_index = self.seed_index if self.seed_index < len(self.seeds) else 0
 				self.selected_seed =  self.seeds[self.seed_index]
 		
+			# interaction
+			if keys[pygame.K_RETURN]:
+				collided_interaction_sprite = pygame.sprite.spritecollide(sprite=self,group=self.interaction,dokill=False)
+				if collided_interaction_sprite:
+					if collided_interaction_sprite[0].name == 'Trader':
+						pass
+					else:
+						self.status = "left_idle"
+						self.sleep = True
+
 	def get_status(self):
 		# idle
 		if self.direction.magnitude() == 0:
